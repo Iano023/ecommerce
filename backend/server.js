bdd3fd9ac74d01be2b7eebfe5a1e9b59146f83c8
Iano023 <ianalbert272@gmail.com>
2025-09-16 22:19:45 +0800

    Signed-off-by: Iano023 <ianalbert272@gmail.com>
    initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv, { configDotenv } from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/auth.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running at port " + PORT);
+
+  connectDB();
+});
